8b32cfffb377080a06e3d4d63e8f5aa2941d2b99
mattacy <115319660+mattacy@users.noreply.github.com>
2022-11-29 11:44:44 -0800

    added password regex, must be 3 chars long - still need to put in the formula

---
+++ pages/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <h1>login here</h1>
+
+    
+
+    <form action="login.php" method="post">
+        Username: <input type="text" name="username" id=""><br>
+        Password: <input type="password" name="password" id=""><br>
+        <input type="submit" value="Login">
+    </form>
+
+    <?php
+
+    // if error checking username and password
+    if(isset($_GET['err'])){
+        echo '<p style="color: red;">',$_GET['err'],'</p>';
+    }
+    ?>
+    
+    <a href="register.php">Click to Register</a>
+    <br>
+           
+    <br>
+    <a href="home.php">Click to go to homepage</a>
+</body>
+</html>
