c559899eda6268ef4eb4c643d5f8d621432b3983
mattacy <115319660+mattacy@users.noreply.github.com>
2022-11-29 23:08:33 -0800

    no delete button, sorted users by type
    took out the delete button and changed selected user to delete user to make it easier

--- pages/admin.php
+++ pages/admin.php
@@ -29,48 +29,58 @@
         echo 'Connection failed' . mysqli_connect_error();
     }
 
-    $sql = "select * from `usertable`";
+    $sql = "SELECT * FROM `usertable` ORDER BY `usertable`.`type` ASC";
     $sqlResult = mysqli_query($conn, $sql);
     $allUsers = mysqli_fetch_all($sqlResult);
+    $disabled = 'disabled';
+    
+    session_start();
+    $_SESSION['type'] = "admin";    // debug, manually set to admin :)
+    
+    if ($_SESSION['type'] == "admin") {
+        $disabled = '';
+    }
+
     ?>
 
     <table>
+    <th>User ID</th>
     <th>Username</th>
     <th>User Type</th>
-    <th>User ID</th>
-    <th>Select User</th>
+    <th>Delete User</th>
     <?php
     foreach($allUsers as $row) {
-        echo "<tr><td>", $row[0], "</td>";      // print user name
+        echo "<tr><td>", $row[3], "</td>";             // print user ID
+        echo "<td>", $row[0], "</td>";      // print user name
 
         // print user type
         if ($row[2] == "user") {     // user type is set
             echo 
-            '<td> <select name="userTypeName" id="type"',$row[3],'>
+            '<td> <select ',$disabled,' name="userTypeName" id="type"',$row[3],'>
                 <option value="userVal">user</option>
                 <option value="modVal">mod</option>
             </select> </td>';
-            } else {      // mod type is set
+            } else if ($row[2] == "mod") {      // mod type is set
                 echo 
-                '<td> <select name="userTypeName" id="type"',$row[3],'>
+                '<td> <select ',$disabled,' name="userTypeName" id="type"',$row[3],'>
                     <option value="modVal">mod</option>
                     <option value="userVal">user</option>
                 </select> </td>';
+            } else if ($row[2] == "admin") {      // mod type is set
+                echo 
+                '<td> <select disabled name="userTypeName" id="type"',$row[3],'>
+                    <option value="adminVal">admin</option>
+                </select> </td>';
             }
     
-        echo "<td>", $row[3], "</td>";             // print user ID
         echo '<td><input type="checkbox" id=user',$row[3],'></td></tr>';        // selection box for delete
     }
     ?>
     </table>
 
-    <br>
-    <button id="deleteButton">Delete Selected</button>
-    <br><br>
-
     <!-- save changes button -->
     <form action="adminSaveFunct.php">
-        <input type="submit" value="Save Changes" />
+        <input type="submit" value="Submit Changes" />
     </form>
     
     <?php
@@ -78,5 +88,6 @@
         echo '<p style="color: red;">',$_GET['msg'],'</p>';
     }
     ?>
+
 </body>
 </html>
