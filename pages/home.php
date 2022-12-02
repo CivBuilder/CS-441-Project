c2fe485a3f7dc5c9486dfa2073d9a69adccc7339
Chris Catechis <catecc1@unlv.nevada.edu>
2022-12-01 18:38:52 -0800

    Revert "pull articles from subscriptions onto home page"
    This reverts commit 42b936893053823f68c2515b22b69441c2ba94b3.

--- pages/home.php
+++ pages/home.php
@@ -17,38 +17,12 @@
     $pass = '';
     $db = 'newsApp';
 
-    // variables for category subscriptions
-    $sports = 0;
-    $business = 0;
-    $technology = 0;
-    $politics = 0;
-
     // connect to db
     $conn =  mysqli_connect('localhost', $user, $pass, $db);
 
     // check connection
     if (!$conn) {
         echo 'Connection failed' . mysqli_connect_error();
-    }
-
-    $sql =  "SELECT `sports`, `business`, `technology`, `politics` FROM `usertable` WHERE `username` = '$username'";
-    $res = mysqli_query($conn, $sql);
-
-    // make an array
-    $subscriptions = mysqli_fetch_all($res);
-    foreach ($subscriptions as $category) {
-        if ($category[0] == 1) {
-            $sports = 1;
-        }
-        if ($category[1] == 1) {
-            $business = 1;
-        }
-        if ($category[2] == 1) {
-            $technology = 1;
-        }
-        if ($category[3] == 1) {
-            $politics = 1;
-        }
     }
     ?>
     <a href="index.php">Click to logout</a>
@@ -76,63 +50,6 @@
     <div>
         <h3>Dashboard</h3>
         <a href="submitArticle.php">Submit Article</a>
-    </div>
-
-    <div>
-        <h1 style='text-align:center'><u>Newsfeed</u></h1>
-        <?php
-        if ($sports == 0 && $business == 0 && $technology == 0 && $politics == 0) {
-            echo "<h2 style='text-align:center'>You don't have any subscriptions yet, search for categories and subscribe to your interests</h2>";
-        } else {
-        ?>
-            <table>
-                <tr>
-                    <td> <b> Article Name</b></td>
-                    <td> <b> Author</b></td>
-                    <td> <b> Category</b></td>
-                </tr>
-                <?php
-                if ($sports == 1) {
-                    $category = 'sports';
-                    $sql = "SELECT `title`, `username`, `category` FROM `newstable` WHERE `category` = '$category'";
-                    $articles = mysqli_query($conn, $sql);
-                    $articlesArray = mysqli_fetch_all($articles, MYSQLI_ASSOC);
-                    foreach ($articlesArray as $row) {
-                        echo '<tr><td> <a href="readArticle.php?article=', $row['title'], '">', $row['title'], "</a></td><td>", $row['username'], "</td><td>", $row['category'], "</td></tr>";
-                    }
-                }
-                if ($business == 1) {
-                    $category = 'business';
-                    $sql = "SELECT `title`, `username`, `category` FROM `newstable` WHERE `category` = '$category'";
-                    $articles = mysqli_query($conn, $sql);
-                    $articlesArray = mysqli_fetch_all($articles, MYSQLI_ASSOC);
-                    foreach ($articlesArray as $row) {
-                        echo '<tr><td> <a href="readArticle.php?article=', $row['title'], '">', $row['title'], "</a></td><td>", $row['username'], "</td><td>", $row['category'], "</td></tr>";
-                    }
-                }
-                if ($technology == 1) {
-                    $category = 'technology';
-                    $sql = "SELECT `title`, `username`, `category` FROM `newstable` WHERE `category` = '$category'";
-                    $articles = mysqli_query($conn, $sql);
-                    $articlesArray = mysqli_fetch_all($articles, MYSQLI_ASSOC);
-                    foreach ($articlesArray as $row) {
-                        echo '<tr><td> <a href="readArticle.php?article=', $row['title'], '">', $row['title'], "</a></td><td>", $row['username'], "</td><td>", $row['category'], "</td></tr>";
-                    }
-                }
-                if ($politics == 1) {
-                    $category = 'politics';
-                    $sql = "SELECT `title`, `username`, `category` FROM `newstable` WHERE `category` = '$category'";
-                    $articles = mysqli_query($conn, $sql);
-                    $articlesArray = mysqli_fetch_all($articles, MYSQLI_ASSOC);
-                    foreach ($articlesArray as $row) {
-                        echo '<tr><td> <a href="readArticle.php?article=', $row['title'], '">', $row['title'], "</a></td><td>", $row['username'], "</td><td>", $row['category'], "</td></tr>";
-                    }
-                }
-                ?>
-            </table>
-        <?php
-        }
-        ?>
     </div>
 </body>
 
